14276aecde583b890f1df7ae7a7ae9fa51456961
Guilherme Moura <mouraggui@gmail.com>
2019-07-27 13:08:18 -0300

    challenge resolution: promise-dot-all

--- promise-dot-all/promise-dot-all.js
+++ promise-dot-all/promise-dot-all.js
@@ -1,5 +1,26 @@
 function all (promises) {
-  return {}
-}
+  return new Promise((resolve, reject) => {
+    let count = 0;
+    const arrPromises = [];
 
-module.exports = all
+    if (!Object.keys(promises).length) {
+      resolve([]);
+    }
+
+    for (let key in promises) {
+      Promise.resolve(promises[key])
+        .then(data => {
+          count++;
+          arrPromises[key] = data;
+
+          if (promises.length === count) {
+            resolve(arrPromises);
+          }
+        }).catch(err => {
+          reject(err);
+        });
+    }
+  });
+};
+
+module.exports = all;
