ed3c919bf5e53e1d918f27ef4db2bfb2437e7ae3
Guilherme Moura <mouraggui@gmail.com>
2019-07-27 20:16:16 -0300

    add-eventing: challenge resolution

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,21 @@
-const addEventing = function (obj) {
-}
+const addEventing = target => {
+  const events = {};
 
-module.exports = addEventing
+  return {
+    ...target,
+    on(name, callback) {
+      if (events[name]) {
+        events[name].push(callback);
+      } else {
+        events[name] = [callback];
+      }
+    },
+    trigger(name, ...args) {
+      if (events[name]) {
+        events[name].map(cb => cb(...args));
+      };
+    },
+  };
+};
+
+module.exports = addEventing;
