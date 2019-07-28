760715bce20141494eaf4087bef72158fa9c3a73
Guilherme Moura <mouraggui@gmail.com>
2019-07-28 08:25:09 -0300

    balanced-parens: code refactoring

--- balanced-parens/balanced-parens.js
+++ balanced-parens/balanced-parens.js
@@ -1,16 +1,24 @@
 const parensAreBalanced = input => {
   const opens = [];
-  const map = { '(': ')', '[': ']', '{': '}', };
-  const parens = [...input].filter(char => /[\(\)\[\]\{\}]/.test(char));
+  const chars = [...input];
+  const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
 
-  for (let i = 0; i < parens.length; i++) {
-    if (/\)\]\}]/.test(parens[i]) && !opens.length) {
+  for (let i = 0; i < chars.length; i++) {
+    if (/\)\]\}]/.test(chars[i]) && !opens.length) {
       return false;
     }
 
-    if (/[\(\[\{]/.test(parens[i])) {
-      opens.unshift(parens[i]);
-    } else if (parens[i] === map[opens[0]]) {
+    if (!/[\(\)\[\]\{\}]/.test(chars[i])) {
+      continue;
+    }
+
+    if (/[\(\[\{]/.test(chars[i])) {
+      opens.unshift(chars[i]);
+    } else if (chars[i] === map[opens[0]]) {
       opens.shift();
     } else {
       return false;
