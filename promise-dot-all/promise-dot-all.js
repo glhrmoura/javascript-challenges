292e1f784679c7e38d60310cb04af15f53424352
Guilherme Moura <mouraggui@gmail.com>
2019-07-28 09:22:44 -0300

    promise-dot-all: added support for Map and Set structures - check if passed value is an Iterable

--- promise-dot-all/promise-dot-all.js
+++ promise-dot-all/promise-dot-all.js
@@ -1,24 +1,25 @@
-function all (promises) {
+const all = iterable => {
   return new Promise((resolve, reject) => {
-    let count = 0;
-    const arrPromises = [];
-
-    if (!Object.keys(promises).length) {
-      resolve([]);
+    if (!iterable[Symbol.iterator]) {
+      reject(new TypeError('object is not iterable (cannot read property Symbol(Symbol.iterator))'));
     }
 
-    for (let key in promises) {
-      Promise.resolve(promises[key])
-        .then(data => {
-          count++;
-          arrPromises[key] = data;
+    let count = 0;
+    const promises = [];
+    const toArr = [...iterable];
 
-          if (promises.length === count) {
-            resolve(arrPromises);
-          }
-        }).catch(err => {
-          reject(err);
-        });
+    if (!toArr.length) resolve([]);
+
+    for (let key in toArr) {
+      let promise = Promise.resolve(toArr[key]);
+
+      promise.then(data => {
+        count++;
+        promises[key] = data;
+        
+        if (toArr.length === count) resolve(promises);
+      })
+      .catch(err => reject(err));
     }
   });
 };
