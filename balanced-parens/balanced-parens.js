26bf05d6c644cefd5dac3818ea23f67261b57f48
Guilherme Moura <mouraggui@gmail.com>
2019-07-27 21:53:51 -0300

    balanced-parens: challenge resolution

--- balanced-parens/balanced-parens.js
+++ balanced-parens/balanced-parens.js
@@ -1,5 +1,26 @@
-const parensAreBalanced = (input) => {
-  return false
+const parensAreBalanced = input => {
+  let open = '';
+  const opens = [];
+  const map = { '(': ')', '[': ']', '{': '}', };
+  const parens = [...input].filter(char => /[\(\)\[\]\{\}]/.test(char));
+
+  for (let i = 0; i < parens.length; i++) {
+    
+    if (/\)\]\}]/.test(parens[i]) && !opens.length) {
+      return false;
+    }
+
+    if (/[\(\[\{]/.test(parens[i])) {
+      open = parens[i];
+      opens.unshift(parens[i]);
+    } else if (parens[i] === map[opens[0]]) {
+      open = opens.shift();
+    } else {
+      return false;
+    }
+  }
+
+  return !opens.length;
 }
 
 module.exports = parensAreBalanced
