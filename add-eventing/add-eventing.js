000521784c367d210d3ec9d6783478ce0d7ecc59
Guilherme Moura <mouraggui@gmail.com>
2019-07-27 22:50:25 -0300

    add-eventing: code refactoring

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,18 +1,20 @@
 const addEventing = target => {
-  const events = {};
+  const listeners = {};
 
   return {
     ...target,
     on(name, callback) {
-      if (events[name]) {
-        events[name].push(callback);
-      } else {
-        events[name] = [callback];
+      if (typeof callback === 'function') {
+        if (!listeners[name]) {
+          listeners[name] = [];
+        }
+
+        listeners[name].push(callback);
       }
     },
     trigger(name, ...args) {
-      if (events[name]) {
-        events[name].map(cb => cb(...args));
+      if (listeners[name]) {
+        listeners[name].map(cb => cb(...args));
       };
     },
   };
