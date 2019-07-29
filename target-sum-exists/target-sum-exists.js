d69bc257c0a707a91c6b6b7d3c8afef5b6edbf5f
Guilherme Moura <mouraggui@gmail.com>
2019-07-28 21:28:29 -0300

    target-sum-exists: challenge resolution

--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -1,12 +1,28 @@
+const checkSum = (arr, crrValue, targetValue, crrDeep, targetDeep) => {
+  crrDeep++;
+  let exists = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (crrDeep < targetDeep) {
+      exists = checkSum(arr, arr[i] + crrValue, targetValue, crrDeep, targetDeep);
+      if (exists) break;
+    } else if (arr[i] + crrValue === targetValue) {
+      exists = true;
+    }
+  }
+
+  return exists;
+};
+
 const targetSumExistsInTwoNumbers = (list, target) => {
-  return false
-}
+  return checkSum(list, 0, target, 0, 2);
+};
 
 const targetSumExistsInThreeNumbers = (list, target) => {
-  return false
-}
+  return checkSum(list, 0, target, 0, 3);
+};
 
 module.exports = {
   targetSumExistsInTwoNumbers,
-  targetSumExistsInThreeNumbers
-}
+  targetSumExistsInThreeNumbers,
+};
