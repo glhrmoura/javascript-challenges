16c3451e8892cf9f4153ab708ad3b26cd77dbdad
Guilherme Moura <mouraggui@gmail.com>
2020-01-22 11:58:59 -0300

    refactoring the Promise.all method

--- promise-dot-all/promise-dot-all.js
+++ promise-dot-all/promise-dot-all.js
@@ -1,4 +1,4 @@
-const all = iterable => {
+const all = (iterable) => {
   return new Promise((resolve, reject) => {
     if (!iterable[Symbol.iterator]) {
       reject(new TypeError('object is not iterable (cannot read property Symbol(Symbol.iterator))'));
@@ -6,20 +6,23 @@ const all = iterable => {
 
     let count = 0;
     const promises = [];
-    const toArr = [...iterable];
+    const entries = Object.entries(iterable)
 
-    if (!toArr.length) resolve([]);
+    if (!entries.length) resolve([]);
 
-    for (let key in toArr) {
-      let promise = Promise.resolve(toArr[key]);
-
-      promise.then(data => {
-        count++;
-        promises[key] = data;
-        
-        if (toArr.length === count) resolve(promises);
-      })
-      .catch(err => reject(err));
+    for (let [ key, value ] of entries) {
+      Promise.resolve(value)
+        .then((data) => {
+          count++;
+          promises[key] = data;
+          
+          if (entries.length === count) {
+            resolve(promises);
+          }
+        })
+        .catch((err) => {
+          reject(err)
+        });
     }
   });
 };
